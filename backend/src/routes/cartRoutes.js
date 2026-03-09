15f575b05d6cee05f386810ca555957d88e22daf
Daniel Dawit <danieldata36@gmail.com>
2026-03-09 23:48:08 +0300

    Update cartRoutes.js

--- backend/src/routes/cartRoutes.js
+++ backend/src/routes/cartRoutes.js
@@ -6,11 +6,8 @@ import {
   updateCartItem,
   removeCartItem,
   clearCart
-
 }from '../controllers/cartController.js';
-
 const router = express.Router();
-
 router.get("/my-cart", protect, getMyCart);
 router.post("/add", protect, addToCart);
 router.patch("/update/:itemId", protect, updateCartItem);
